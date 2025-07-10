1fd9fe8f80085ba046813877254db7d33bb17856
MarkMolabola <mark.molabola@outlook.com>
2025-07-10 10:27:33 -0700

    updated

--- Student.cs
+++ Student.cs
@@ -6,41 +6,19 @@ using System.Threading.Tasks;
 
 namespace Assignment_1._4._2
 {
-    internal class Student
+    public class Student
     {
-      
-            private int studentId;
-            private string studentFirstName;
-            private string studentLastName;
-            private char studentGrade;
+        public int StudentId { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+        public char StudentGrade { get; set; }
+        public Student(int studentid, string firstname, string lastname, char grade) //constructor
+        {
+            StudentId = studentid;
+            StudentFirstName = firstname;
+            StudentLastName = lastname;
+            StudentGrade = grade;
+        }
 
-            public Student(int studentid, string firstname, string lastname, char grade) //constructor
-            {
-                studentId = studentid;
-                studentFirstName = firstname;
-                studentLastName = lastname;
-                studentGrade = grade;
-            }
-            public int StudentId
-            {
-                get { return studentId; }
-                set { studentId = value; }
-            }
-            public string StudentFirstName
-            {
-                get { return studentFirstName; }
-                set { studentFirstName = value; }
-            }
-            public string StudentLastName
-            {
-                get { return studentLastName; }
-                set { studentLastName = value; }
-            }
-            public char StudentGrade
-            {
-                get { return studentGrade; }
-                set { studentGrade = value; }
-            }
-        
     }
 }
